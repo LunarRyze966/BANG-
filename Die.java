fcc5e96d85253d1a304c3c2e0cffdb9846d7f9d0
Damian Reyes <61527378+LunarRyze966@users.noreply.github.com>
2020-05-05 18:51:28 -0500

    Add files via upload
    Interprets each die as a separate roll method rather than defining them through the die constructor

--- Die.java
+++ Die.java
@@ -9,124 +9,100 @@ public class Die{
     
     private final int MAX = 6;
     private String faceValue;
-    private String dieType;
-    static String[] possibleDieTypes = {"Basic","Coward","Loudmouth","Duel"};
-            
+    
     //Die constructor
     public Die(){
         faceValue = "";
-        dieType = "";
     }
     
     //Returns string representing the action for each roll
-    public void roll(){      
+    public String basicRoll(){
         int value;
         value = (int)(Math.random()*6)+1;
         switch (value){
             case 1:
-                if(dieType.equals("Basic")){
-                    faceValue = "Arrow";
-                }
-                else if(dieType.equals("Coward")){
-                    faceValue = "Arrow";
-                }
-                else if(dieType.equals("Loudmouth")){
-                    faceValue = "Arrow";
-                }
-                else if(dieType.equals("Duel")){
-                    faceValue = "Arrow";
-                }
-                break;
+                return "Arrow";
             case 2:
-                if(dieType.equals("Basic")){
-                    faceValue = "Dynamite";
-                }
-                else if(dieType.equals("Coward")){
-                    faceValue = "Dynamite";
-                }
-                else if(dieType.equals("Loudmouth")){
-                    faceValue = "Dynamite";
-                }
-                else if(dieType.equals("Duel")){
-                    faceValue = "Dynamite";
-                }
-                break;
+                return "Dynamite";
             case 3:
-                if(dieType.equals("Basic")){
-                    faceValue = "Bull's Eye 1";
-                }
-                else if(dieType.equals("Coward")){
-                    faceValue = "Bull's Eye 1";
-                }
-                else if(dieType.equals("Loudmouth")){
-                    faceValue = "Double Bull's Eye 1";
-                }
-                else if(dieType.equals("Duel")){
-                    faceValue = "Whiskey Bottle";
-                }
-                break;
+                return "Bull's Eye 1";
             case 4:
-                if(dieType.equals("Basic")){
-                    faceValue = "Bulls Eye 2";
-                }
-                else if(dieType.equals("Coward")){
-                    faceValue = "Broken Arrow";
-                }
-                else if(dieType.equals("Loudmouth")){
-                    faceValue = "Double Bull's Eye 2";
-                }
-                else if(dieType.equals("Duel")){
-                    faceValue = "Gatling";
-                }
-                break;
+                return "Bull's Eye 2";
             case 5:
-                if(dieType.equals("Basic")){
-                    faceValue = "Beer";
-                }
-                else if(dieType.equals("Coward")){
-                    faceValue = "Beer";
-                }
-                else if(dieType.equals("Loudmouth")){
-                    faceValue = "Bullet";
-                }
-                else if(dieType.equals("Duel")){
-                    faceValue = "Fight a Duel";
-                }
-                break;
+                return "Beer";
             case 6:
-                if(dieType.equals("Basic")){
-                    faceValue = "Gatling";
-                }
-                else if(dieType.equals("Coward")){
-                    faceValue = "Double Beer";
-                }
-                else if(dieType.equals("Loudmouth")){
-                    faceValue = "Double Gatling";
-                }
-                else if(dieType.equals("Duel")){
-                    faceValue = "Fight a Duel";
-                }
-                break;
-
-                
+                return "Gatling";        
         }
+        return "";
     }
+    
+    public String cowardRoll(){
+        int value;
+        value = (int)(Math.random()*6)+1;
+        switch (value){
+            case 1:
+                return "Arrow";
+            case 2:
+                return "Dynamite";
+            case 3:
+                return "Bull's Eye 1";
+            case 4:
+                return "Broken Arrow";
+            case 5:
+                return "Beer";
+            case 6:
+                return "Double Beer";
+        }
+        return "";
+    }
+    
+    public String loudmouthRoll(){
+        int value;
+        value = (int)(Math.random()*6)+1;
+        switch (value){
+            case 1:
+                return "Arrow";
+            case 2:
+                return "Dynamite";
+            case 3:
+                return "Double Bull's Eye 1";
+            case 4:
+                return "Double Bull's Eye 2";
+            case 5:
+                return "Bullet";
+            case 6:
+                return "Double Gatling";
+        }
+        return "";
+    }
+    
+    public String duelRoll(){     
+        int value;
+        value = (int)(Math.random()*6)+1;
+        switch (value){
+            case 1:
+                return "Arrow";
+            case 2:
+                return "Dynamite";
+            case 3:
+                return "Whiskey Bottle";
+            case 4:
+                return "Gatling";
+            case 5:
+                return "Fight a Duel";
+            case 6:
+                return "Fight a Duel";
+        }
+        return "";
+    }
+    
     // Mutator for Die FaceValue
     public void setFaceValue (String value){
         faceValue = value;
     }
     
     //Accessor for Die FaceValue
-    public String getFaceValue(){
+    public String FaceValue(){
         return faceValue;
-    }
-    
-    // Mutator for Die DieType
-    public void setDieType(String type){
-        dieType = type;
-    }
-    
-    public String getDieType(){
-        return dieType;
     }
 }
